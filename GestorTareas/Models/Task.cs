d1e6e0d9a6516a7f8951b7f15bce3f2d06624364
kenthLol <kenthsamu@gmail.com>
2025-11-12 11:19:13 -0600

    chore: modify .gitignore and add basic project structure

---
+++ GestorTareas/Models/Task.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestorTareas.Models;
+
+public class Task
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public DateTime CreationDate { get; set; } = DateTime.Now;
+    public bool IsCompleted { get; set; } = false;
+}
