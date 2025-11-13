d991d267ceb8baf6ab45a6e730c9e40ea988d6c0
kenthLol <kenthsamu@gmail.com>
2025-11-13 16:11:17 -0600

    feat: Add priority selection, search, and filter options to console menu

--- GestorTareas/Program.cs
+++ GestorTareas/Program.cs
@@ -1,4 +1,5 @@
 ﻿using GestorTareas;
+using GestorTareas.Models;
 
 TaskManager taskManager = new TaskManager();
 
@@ -9,7 +10,9 @@ while(true)
     Console.WriteLine("2. Listar tareas");
     Console.WriteLine("3. Completar tarea");
     Console.WriteLine("4. Eliminar tarea");
-    Console.WriteLine("5. Salir");
+    Console.WriteLine("5. Buscar tarea");
+    Console.WriteLine("6. Ordenar tareas por prioridad");
+    Console.WriteLine("7. Salir");
     Console.Write("Selecciona una opción: ");
 
     string? input = Console.ReadLine();
@@ -21,17 +24,28 @@ while(true)
         case "1":
             Console.Write("Titulo: ");
             string? title = Console.ReadLine();
+
             Console.Write("Descripcion: ");
             string? description = Console.ReadLine();
-            if(!string.IsNullOrWhiteSpace(title) && !string.IsNullOrWhiteSpace(description))
+
+            Console.Write("Prioridad (1-Baja, 2-Media, 3-Alta): ");
+            string? priorityStr = Console.ReadLine();
+
+            if(!int.TryParse(priorityStr, out int priorityInput) ||  !Enum.IsDefined(typeof(Priority), priorityInput))
             {
-                taskManager.AddWorkTask(title, description);
+                priorityInput = (int)Priority.Media;
+            }
+
+            if (!string.IsNullOrWhiteSpace(title) && !string.IsNullOrWhiteSpace(description))
+            {
+                taskManager.AddWorkTask(title, description, (Priority)priorityInput);
                 Console.WriteLine("Tarea asignada correctamente.");
             }
             else
             {
                 Console.WriteLine("El titulo y la descripcion no pueden estar vacios");
             }
+
             taskManager.CleanScreen();
             break;
         case "2":
@@ -40,26 +54,53 @@ while(true)
             break;
         case "3":
             Console.WriteLine("Id de la tarea a completar: ");
+
             if(int.TryParse(Console.ReadLine(), out int idCompletar))
             {
                 taskManager.CompleteWorkTask(idCompletar);
             }
+
             taskManager.CleanScreen();
             break;
         case "4":
             Console.WriteLine("Id de la tarea a eliminar: ");
+
             if(int.TryParse(Console.ReadLine(), out int idEliminar))
             {
                 taskManager.DeleteWorkTask(idEliminar);
             }
+
             taskManager.CleanScreen();
             break;
         case "5":
+            Console.WriteLine("Buscar (Titulo o Descripcion): ");
+            string text = Console.ReadLine() ?? string.Empty;
+            taskManager.FindWorkTask(text);
+
+            taskManager.CleanScreen();
+            break;
+
+        case "6":
+            Console.WriteLine("Buscar por Prioridad (1.Baja, 2.Media, 3.Alta): ");
+            string? priorityStrSort = Console.ReadLine();
+
+            if(!int.TryParse(priorityStrSort, out int priority) ||  !Enum.IsDefined(typeof(Priority), priority))
+            {
+                priority = (int)Priority.Media;
+            }
+
+            taskManager.SortWorkTasksByPriority((Priority)priority);
+
+            taskManager.CleanScreen();
+            break;
+        case "7":
             Console.WriteLine("Hasta luego!");
+
             taskManager.CleanScreen();
             return;
         default:
             Console.WriteLine("Opción inválida.");
+
             taskManager.CleanScreen();
             break;
 
