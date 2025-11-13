a839813507c2941d443b658c972ac5f83d416a02
kenthLol <kenthsamu@gmail.com>
2025-11-13 16:09:28 -0600

    feat: Add Priority property to WorkTask model

--- GestorTareas/Models/WorkTask.cs
+++ GestorTareas/Models/WorkTask.cs
@@ -9,6 +9,7 @@ public class WorkTask
     public int Id { get; set; }
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
+    public Priority Priority { get; set; } = Priority.Media;
     public DateTime CreationDate { get; set; } = DateTime.Now;
     public bool IsCompleted { get; set; } = false;
 }
